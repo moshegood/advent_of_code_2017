eec7f8015866d7fc94def273f43d795a1544dfa5
moshe.good <moshe.good@wishabi.com>
2017-12-06 09:32:58 -0500

    Day 06 challenges

---
+++ 06/part2.pl
@@ -0,0 +1,31 @@
+#! /usr/bin/perl
+use strict;
+use warnings;
+
+while(my $line = <>) {
+    chomp $line;
+    my @ns = split(' ', $line);
+    my $length = scalar @ns;
+
+    my $steps = 1;
+    my %seen;
+    while(!$seen{"@ns"}) {
+        $seen{"@ns"} = $steps;
+
+        my $max = 0;
+        my $idx = 0;
+        while( $idx < $length ) {
+            $max = $idx if $ns[$idx] > $ns[$max];
+            $idx++;
+        }
+
+        my $x = $ns[$max];
+        $ns[$max] = 0;
+        foreach my $i (1 .. $x){
+            $ns[($max + $i) % $length]++;
+        }
+
+        $steps++;
+    }
+    print $steps - $seen{"@ns"},"\n";
+}
